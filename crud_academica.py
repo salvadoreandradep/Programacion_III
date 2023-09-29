d0ddfd61fca44641d8711008bb12af33433b0734
cipher <salvadoplay7089@gmail.com>
2023-09-28 19:20:58 -0600

    Crud Finalizado

--- crud_academica.py
+++ crud_academica.py
@@ -15,6 +15,15 @@ class crud:
             print("Error de conexion")
 
     def consultar(self, sql):
-        cursor = self.conexion.cursor()
+        cursor = self.conexion.cursor(dictionary=True)
         cursor.execute(sql)
         return cursor.fetchall()
+    
+    def ejecutar_consultas(self, sql, val):
+        try:
+            cursor = self.conexion.cursor()
+            cursor.execute(sql, val)
+            self.conexion.commit()
+            return "ok"
+        except Exception as e:
+            return str(e)
