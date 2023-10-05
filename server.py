97004d1299de8aebd1573bb0f07733227337648b
mariaesalgado <salvadoplay7089@gmail.com>
2023-10-05 00:52:25 -0600

    CRUD

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from urllib import parse
-import mysql.connector
+import crud_alumnos
+import json
+
+crud_alumnos = crud_alumnos.crud_alumnos()
 port = 3000
 
 class miServer(SimpleHTTPRequestHandler):
@@ -8,15 +11,32 @@ class miServer(SimpleHTTPRequestHandler):
         if self.path=="/":
             self.path = "index.html"
             return SimpleHTTPRequestHandler.do_GET(self)
+        
+        if self.path=="/frmalumnos":
+            self.path = "alumnos.html"
+            return SimpleHTTPRequestHandler.do_GET(self)
+        
+        if self.path=="/frmbusqueda_alumnos":
+            self.path = "busqueda_alumnos.html"
+            return SimpleHTTPRequestHandler.do_GET(self)
+       
+        if self.path=="/alumnos":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(json.dumps(crud_alumnos.consultar_alumnos()).encode('utf-8'))
+            
     def do_POST(self):
         longitud = int(self.headers["Content-Length"])
         datos= self.rfile.read(longitud)
         datos = datos.decode()
         datos = parse.unquote(datos)
+        datos = json.loads(datos)
+        if self.path=="/alumnos":
+            resp = {"msg": crud_alumnos.administrar(datos)}
         
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(datos.encode())
+        self.wfile.write(json.dumps(resp).encode())
         
 print("Ejecuntando server en puerto ", port)
 server = HTTPServer(("localhost", port), miServer)
