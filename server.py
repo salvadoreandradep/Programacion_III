07e1b936bde87f372dc17b05057d6251a1cc05cd
salvadoreandradep <salvadorplay7089@gmail.com>
2023-10-17 18:10:18 -0600

    Login

--- server.py
+++ server.py
@@ -4,13 +4,39 @@ from mysql.connector import Error
 
 app = Flask(__name__, template_folder='templates')
 
+db = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="db_freund"
+)
+
 
 @app.route('/')
 def formulario():
     return render_template('principal.html')
 
 
+@app.route('/login')
+def loginP():
+    return render_template('login.html')
 
+@app.route('/register')
+def Inicio():
+    return render_template('inicio.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM usuarios WHERE nombre_usuario = %s AND contrasena = %s", (username, password))
+    user = cursor.fetchone()
+    if user:
+        return redirect(url_for('Inicio'))
+    else:
+        return "Usuario o contraseña incorrectos"
 
 
 
