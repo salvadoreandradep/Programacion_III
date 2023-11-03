82a540185b5d7cba0b79848a249e307c6ecf926b
salvadoreandradep <salvadorplay7089@gmail.com>
2023-11-02 23:19:23 -0600

    SQLite

--- proyecto_django/app_django/models.py
+++ proyecto_django/app_django/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class alumnos(models.Model):
+    codigo= models.CharField(max_length=10)
+    nombre= models.CharField(max_length=65)
+    telefono= models.CharField(max_length=9)
