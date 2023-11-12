7e8b3d4d8cd4fe00cd5454a81c38b530d94ffe50
salvadoreandradep <salvadorplay7089@gmail.com>
2023-11-11 18:38:11 -0600

    Eliminar funcionando

--- server.py
+++ server.py
@@ -16,6 +16,7 @@ db = mysql.connector.connect(
 cursor = db.cursor()
 
 ## Administracion de productos................................................................
+
 @app.route('/producto')
 def mostrar_productos():
     # Obtener productos de la base de datos
@@ -40,120 +41,24 @@ def guardarP():
 
     return redirect('/producto')
 
+@app.route('/eliminar/<int:producto_id>', methods=['POST'])
+def eliminar_producto(producto_id):
+    # Eliminar producto de la base de datos
+    delete_query = "DELETE FROM Productos WHERE idProducto = %s"
+    cursor.execute(delete_query, (producto_id,))
+    db.commit()
 
+    return redirect('/producto')
 
 
 
 ##Administracion de clientes.......................................................................
 
-@app.route('/guardarC', methods=['POST'])
-def guardar():
-    cursor = db.cursor()
-    codigo = request.form['txtCodigoAlumnos']
-    nombre = request.form['txtNombreAlumnos']
-    direccion = request.form['txtDireccionAlumnos']
-    telefono = request.form['txtTelefonoAlumnos']
-
-
-    query = "INSERT INTO clientes (nombre, apellido, dui, telefono) VALUES (%s, %s, %s, %s)"
-    cursor.execute(query, (codigo, nombre, direccion, telefono))
-    db.commit()
-    cursor.close()
-
-    return redirect(url_for('clientes'))
-
-
-@app.route('/cliente')
-def tabla_alumnos():
-    cursor = db.cursor()
-    cursor.execute("SELECT nombre, apellido, dui , telefono FROM clientes")
-    alumnos = cursor.fetchall()
-    return render_template('clientes.html', alumnos=alumnos)
-
-
-@app.route('/eliminar_cliente', methods=['POST'])
-def eliminar_alumno():
-    codigo = request.form['codigo']
-    cursor = db.cursor()
-    cursor.execute("DELETE FROM `clientes` WHERE nombre =  %s", (codigo,))
-    db.commit()
-    return redirect('/cliente')
 
 
 
 ## Administracion de Empleados................................................................
 
-@app.route('/guardarE', methods=['POST'])
-def guardarE():
-    cursor = db.cursor()
-    codigo = request.form['txtCodigoAlumnos']
-    nombre = request.form['txtNombreAlumnos']
-    direccion = request.form['txtDireccionAlumnos']
-    telefono = request.form['txtTelefonoAlumnos']
-
-
-    query = "INSERT INTO empleados (nombre, apellido, area, turno) VALUES (%s, %s, %s, %s)"
-    cursor.execute(query, (codigo, nombre, direccion, telefono))
-    db.commit()
-    cursor.close()
-
-    return redirect(url_for('empleado'))
-
-
-@app.route('/empleado')
-def tabla_empleados():
-    cursor = db.cursor()
-    cursor.execute("SELECT nombre, apellido, area, turno FROM empleados")
-    alumnos = cursor.fetchall()
-    return render_template('empleados.html', alumnos=alumnos)
-
-
-
-@app.route('/eliminar_empleado', methods=['POST'])
-def eliminar_pempleado():
-    codigo = request.form['codigo']
-    cursor = db.cursor()
-    cursor.execute("DELETE FROM `empleados` WHERE nombre =  %s", (codigo,))
-    db.commit()
-    return redirect('empleado')
-
-
-
-
-
-
-
-@app.route("/reportetabla")
-def view_reports():
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM `problemas`")
-    reports = cursor.fetchall()
-    return render_template("reportetabla.html", reports=reports)
-
-
-@app.route('/eliminar_reporte', methods=['POST'])
-def eliminar_reporte():
-    codigo = request.form['codigo']
-    cursor = db.cursor()
-    cursor.execute("DELETE FROM `problemas` WHERE id =  %s", (codigo,))
-    db.commit()
-    return redirect('/reportetabla')
-
-
-
-@app.route("/submit_report", methods=["POST"])
-def submit_report():
-    hora = request.form["hora"]
-    dia = request.form["dia"]
-    problema = request.form["problema"]
-    empleado_nombre = request.form["empleado_nombre"]
-
-    cursor = db.cursor()
-    insert_query = "INSERT INTO problemas (hora, dia, problema, empleado_nombre) VALUES (%s, %s, %s, %s)"
-    cursor.execute(insert_query, (hora, dia, problema, empleado_nombre))
-    db.commit()
-
-    return redirect(url_for("Inicio"))
 
 
 @app.route('/login', methods=['POST'])
